d6c09d8cf22a0b7e0ae4502c83eb94237788a9a8
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-11-18 22:35:55 +0000

    Module Documentation

--- mod.ts
+++ mod.ts
@@ -1,3 +1,26 @@
+// Copyright 2024 The Neuf authors. MIT License.
+/**
+ * A minimal SSR "framework" for building web applications using Deno.
+ * Uses JSX for defining pages, layouts, and components.
+ * Includes a file-based router similar to Next.js, and support for middleware.
+ *
+ * Is very early in development, and is not yet recommended for production use.
+ * Expect breaking changes in future releases. You have been warned!
+ *
+ * _Module documentation will be added in a future release._
+ * _Most exposed symbols are lightly documented with JSDoc._
+ *
+ * Coming soon:
+ * - Better documentation!
+ * - Examples
+ * - Control over when promises are resolved (e.g. no need to block for a network request which has no effect on the page render)
+ * - First-party error handling
+ * - Dedicated API route-handlers
+ * - Dedicated solution for i18n, among other things
+ *
+ * @module
+ */
+
 export * as Constants from "./src/dev/constants.ts";
 export * as Decorators from "./src/decorators/lib.ts";
 export * as Host from "./src/host/index.ts";
