188ba4cab0212ccffaa69af4eaa8a67c80d0fa7c
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-11-29 13:51:28 +0000

    Reimplemented Decorators

---
+++ src/features/decorators.ts
@@ -0,0 +1,9 @@
+import type * as Lib from "../lib/types.ts";
+
+export function NeufLayout(constructor: Lib.LayoutClass): void {
+    constructor;
+}
+
+export function NeufPage(constructor: Lib.PageClass): void {
+    constructor;
+}
