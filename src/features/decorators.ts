474f9e19b99ffc77058a07fb4e37a79cbc659209
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-12-02 16:32:36 +0000

    Expose Decorators

--- src/features/decorators.ts
+++ src/features/decorators.ts
@@ -1,3 +1,9 @@
+/**
+ * Provides decorators for type-checking Neuf layouts and pages.
+ *
+ * @module
+ */
+
 import type * as Lib from "../lib/types.ts";
 
 export function NeufLayout(constructor: Lib.LayoutClass): void {
